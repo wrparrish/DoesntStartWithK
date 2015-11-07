9b81ca50a91184e340b501223ef5343bd06fd255
William Parrish <william@bytemark.co>
2015-11-07 17:45:48 -0500

    fleshing out the rv creation

--- app/src/main/java/com/ruthlessprogramming/doesntstartwithk/Presenter/list_movies/rec_view/MovieAdapter.kt
+++ app/src/main/java/com/ruthlessprogramming/doesntstartwithk/Presenter/list_movies/rec_view/MovieAdapter.kt
@@ -1,10 +1,10 @@
 package com.ruthlessprogramming.doesntstartwithk.Presenter.list_movies.rec_view
 
 import android.content.Context
-import android.graphics.Movie
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import com.ruthlessprogramming.doesntstartwithk.Model.Movie
 
 import com.ruthlessprogramming.doesntstartwithk.R
 
